470b84632a9c179c0c01c09dd0e710ecb8f62e80
Ambuj Kumar Maurya <ambuj20maurya@gmail.com>
2026-06-01 22:35:15 +0530

    Add DB_PORT to support Aiven

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 // Create a connection pool instead of a single connection
 // This is industry standard as it handles multiple concurrent queries better
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'food_delivery_app',
